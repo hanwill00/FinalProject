4ee36f060c302eaea0d6ce96f6cb375494f33aaf
Will Han <whan2@swarthmore.edu>
2018-04-26 23:51:16 -0400

    sadly must stop here

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,5 +1,6 @@
 class Itinerary < ApplicationRecord
-  has_many :events, dependent: :destroy
+  has_many :events, inverse_of: :itinerary
+  accepts_nested_attributes_for :events, reject_if: :all_blank
   belongs_to :user
   validates :name, presence: true
 end
